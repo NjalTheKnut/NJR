9647868625fc74972fe38621956ea0c6bc3a6c69
Neal Robison <nealjrobison@gmail.com>
2018-04-20 12:30:48 -0400

    Personal Website

---
+++ NJR_Page_01/NJR_Page_Scripts.js
@@ -0,0 +1,11 @@
+function func1(str){
+	alert(str);
+}
+
+var message = "Welcome to Neal's Page!"
+window.onload(func1(message));
+
+function func2(){
+	var leavePageMessage ="This link will open in a new tab."
+	func1(leavePageMessage);
+}
