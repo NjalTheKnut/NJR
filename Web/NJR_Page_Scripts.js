2d06b909d3a7f95b06b66d1c724b94b178a50264
Neal R <nealjrobison@gmail.com>
2018-06-20 11:51:57 -0400

    script changes
    - major js overhaul

--- Web/NJR_Page_Scripts.js
+++ Web/NJR_Page_Scripts.js
@@ -1,39 +1,42 @@
-var message = "Welcome to Neal's Page!"
-function func1(str){
-	alert(str);
-}
-function func2(){
-	var leavePageMessage ="This link will open in a new tab."
-	func1(leavePageMessage);
-}
-window.onload(func1(message));
+window.onload = function(){
+	var welcome = "Welcome to Neal's Page!"
+	alert(welcome);
 
-var images = [
-	'https://njaltheknut.github.io/NJR/Web/cert-1014-7239257.jpg',
-	'https://njaltheknut.github.io/NJR/Web/cert-1023-7239257.jpg',
-	'https://njaltheknut.github.io/NJR/Web/cert-1024-7239257.jpg',
-	'https://njaltheknut.github.io/NJR/Web/cert-1051-7239257.jpg',
-	'https://njaltheknut.github.io/NJR/Web/cert-1059-7239257.jpg',
-	'https://njaltheknut.github.io/NJR/Web/cert-1060-7239257.jpg',
-	'https://njaltheknut.github.io/NJR/Web/cert-1068-7239257.jpg',
-	'https://njaltheknut.github.io/NJR/Web/cert-1073-7239257.jpg',
-	'https://njaltheknut.github.io/NJR/Web/cert-1080-7239257.jpg',
-	'https://njaltheknut.github.io/NJR/Web/cert-1081-7239257.jpg'
-];
-var num = 0;
-function next(){
-	var slider = document.getElementById('slider');
-	num++;
-	if(num >= images.length){
-		num = 0;
+	function goodbye(){
+		var leavePageMessage = "This link will open in a new tab."
+		alert(leavePageMessage);
 	}
-	slider.src = images[num];
-}
-function prev(){
-	var slider = document.getElementById('slider');
-	num--;
-	if(num < 0){
-		num = images.length-1;
+
+	var images = [
+		'https://njaltheknut.github.io/NJR/Web/cert-1014-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1023-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1024-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1051-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1059-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1060-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1068-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1073-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1080-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1081-7239257.jpg'
+	];
+	var num = 0;
+	var prevButton = document.getElementById('prevButton');
+	prevButton.onclick = function(){
+		var slider = document.getElementById('slider');
+		num--;
+		if(num < 0){
+			num = images.length-1;
+		}
+		slider.src = images[num];
+		
 	}
-	slider.src = images[num];
+	var nextButton = document.getElementById('nextButton');
+	nextButton.onclick = function(){
+		var slider = document.getElementById('slider');
+		num++;
+		if(num >= images.length){
+			num = 0;
+		}
+		slider.src = images[num];
+	}
 }
